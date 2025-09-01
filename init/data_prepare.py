e13e247f5bda64fe7e0a2147f49813f23683d047
zsy <875508913@qq.com>
2025-09-01 12:47:45 +0800

    1

--- init/data_prepare.py
+++ init/data_prepare.py
@@ -524,5 +524,5 @@ if __name__ == "__main__":
     # print(fetch_trade_dates())
     # update_stock_daily_kline(process=True,force_update=False)
     # print(DB_PATH)
-    # update_stock_kline(freq='daily')
-    get_stock_industry()
+    update_stock_kline(freq='daily')
+    # get_stock_industry()
