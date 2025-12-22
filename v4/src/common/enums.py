f5e5632bf6f66e5e344def6188d56ea94e5a04c1
zhangsongyu <875508913@qq.com>
2025-12-22 21:18:43 +0800

    1

--- v4/src/common/enums.py
+++ v4/src/common/enums.py
@@ -1,21 +1,16 @@
-from enum import Enum, IntEnum
+from enum import Enum
 
-class Exchange(Enum):
-    """交易所标准定义"""
-    SZSE = "SZ"  # 深交所
-    SSE = "SH"   # 上交所
-    BSE = "BJ"   # 北交所
 
-class AdjustType(Enum):
-    """复权类型"""
-    NONE = "none"   # 不复权 (实盘用)
-    PRE = "qfq"     # 前复权 (回测默认)
-    POST = "hfq"    # 后复权
-    
-# 核心字段名映射（防止拼写错误）
-class Fields:
-    SYMBOL = "symbol"
+class Fields(str, Enum):
+    """
+    DataFrame的列名
+    """
     DT = "datetime"
-    T = 'time'
+    T = 'Time'
+    OPEN = "open"
+    HIGH = "high"
+    LOW = "low"
+    CLOSE = "close"
     VOL = "volume"
     AMT = "amount"
+    SYMBOL = "symbol"
