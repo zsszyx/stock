9c4e3fe457371384de3bfcfa9f3094b7e0f08f5f
zhangsongyu <875508913@qq.com>
2026-02-02 20:03:53 +0800

    1

--- task/update_task.py
+++ task/update_task.py
@@ -121,4 +121,4 @@ class UpdateTask:
 if __name__ == '__main__':
     task = UpdateTask()
     # Example usage
-    task.run_init_mintues5_task(start_date='2025-12-01', end_date='2026-01-29')
+    task.run_init_mintues5_task(start_date='2025-12-01', end_date='2026-02-02')
